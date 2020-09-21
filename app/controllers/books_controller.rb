f50b92bfc52247a1a997f93e6a51f63b943d22b8
EC2 Default User <ec2-user@ip-172-31-27-169.ec2.internal>
2020-09-21 09:39:16 +0000

    [Add]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,48 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.order(id: :"desc")
+    @books = Book.all.order(id: :"asc")
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    flash[:notice] = "successfully posted"
+    else
+    @books = Book.all.order(id: :"asc")
+    render "index"
+    end
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "successfully updated"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "successfully deleted"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
